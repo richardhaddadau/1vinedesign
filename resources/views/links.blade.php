69af73040cac1bbda9a3355b015c4a9884335060
Richard Haddad <richard@haddads.net.au>
2022-05-25 11:20:59 +1000

    Fixed links in /top-links

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -25,10 +25,10 @@
         <article class="hero-content make-centered">
             <a href="https://calendly.com/1vinedesign"><h1 class="make-white make-links-button make-links-full">Free Discovery Session</h1></a>
             <a href="/"><h1 class="make-white make-links-button make-links-full">Our Website</h1></a>
-            <a href=""><h1 class="make-white make-links-button">Lowest Domain Name Prices</h1></a>
-            <a href=""><h1 class="make-white make-links-button">Cloud Hosting</h1></a>
-            <a href=""><h1 class="make-white make-links-button">Premium SSL Certificates</h1></a>
-            <a href=""><h1 class="make-white make-links-button">Shared Web Hosting</h1></a>
+            <a href="https://www.1vinehosting.com.au/"><h1 class="make-white make-links-button">Lowest Domain Name Prices</h1></a>
+            <a href="https://www.cloudways.com/en/?id=931014"><h1 class="make-white make-links-button">Performance Cloud Hosting</h1></a>
+            <a href="https://www.1vinehosting.com.au/ssl-certificates/"><h1 class="make-white make-links-button">Premium SSL Certificates</h1></a>
+            <a href="https://www.1vinehosting.com.au/web-hosting/"><h1 class="make-white make-links-button">Shared Web Hosting</h1></a>
         </article>
     </section>
 </header>
